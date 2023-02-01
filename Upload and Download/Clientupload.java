2acb7dbbde870e9bad0dd514d7bb75a3c6d4eb27
sathiyaseelan1200 <95630203+sathiyaseelan1200@users.noreply.github.com>
2023-02-01 11:03:48 +0530

    Write a HTTP web client program to download a web page using TCP sockets

---
+++ Upload and Download/Clientupload.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.net.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException; import
+javax.imageio.ImageIO;
+public class ClientUpload{
+
+public static void main(String args[]) throws Exception{ Socket soc;
+BufferedImage img = null;
+soc=new Socket("localhost",5000);
+
+System.out.println("Client is running. ");
+
+System.out.println("Reading image from disk. ");
+
+img = ImageIO.read(new File("digital_image_processing.jpg")); 
+ByteArrayOutputStream baos = new ByteArrayOutputStream();
+ImageIO.write(img, "jpg", baos);
+baos.flush();
+
+byte[]  bytes = baos.toByteArray();
+baos.close();
+} 
+} 
